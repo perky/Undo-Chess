66d0116dec664cd27f24221f92c7423a402b7322
Luke Perkin <lukeperkin@gmail.com>
2010-11-19 16:39:42 +0000

    Mac OS X release, v1.4.

--- conf.lua
+++
@@ -1,4 +0,0 @@
-function love.conf(t)
-    t.screen.width = 700
-    t.screen.height = 500
-end
--- goo/objects/null.lua
+++
@@ -1,7 +0,0 @@
--- NULL OBJECT
-goo.null = class('goo null', goo.object)
-function goo.null:initialize( parent )
-	super.initialize(self)
-end
-
-return goo.null
